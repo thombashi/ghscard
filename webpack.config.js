54a1b33ed4f30cf52a6a77e593043c8499857439
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-07-15 08:41:51 +0900

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-let path = require('path');
-let webpack = require('webpack');
+let path = require("path");
+let webpack = require("webpack");
 
 module.exports = {
-    entry: path.join(__dirname, 'js', 'main.js'),
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'ghscard.js'
-    },
-    devtool: 'source-map'
-}
+  mode: "production",
+  entry: path.join(__dirname, "js", "main.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "ghscard.js"
+  },
+  devtool: "source-map"
+};
