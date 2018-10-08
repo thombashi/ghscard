6734e3c027c38e1425af8bccc8acc24698204c36
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-08 18:30:12 +0900

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -88,6 +88,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development",
         "Topic :: Software Development :: Widget Sets",
     ],
