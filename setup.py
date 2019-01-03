1ee78dee7803b9b33cc4f97d1093eb5470eae369
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-01-03 15:06:34 +0900

    Change development status from alpha to beta

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setuptools.setup(
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
 
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
