11268e47d7a5f7106fc12f9ba67ca0932abca688
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-05-02 17:50:30 +0900

    Drop support for Python 3.4

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setuptools.setup(
         "release": ["releasecmd>=0.0.18,<0.1.0"],
         "test": tests_requires,
     },
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
 
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -88,7 +88,6 @@ setuptools.setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
