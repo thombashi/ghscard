1bc22c2546c92918d563758b79c5aec0bfca51cf
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-12 08:01:33 +0900

    Update a utility script

--- fetch_js_lib_versions.sh
+++ fetch_js_lib_versions.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-for pkg in Chart.js jquery moment.js semantic-ui pleasejs; do
+for pkg in moment.js pleasejs; do
     curl -sS https://api.cdnjs.com/libraries/${pkg}?fields=name,filename,version | jq .
+done
+
+for pkg in chart.js jquery fomantic-ui; do
+    echo $pkg $(curl -sS https://data.jsdelivr.com/v1/package/npm/${pkg} | jq '.["tags"]["latest"]')
 done
