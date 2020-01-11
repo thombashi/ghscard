352c9212d69fcd9dfb796d83ee9d66d125116180
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-12 08:38:45 +0900

    Update js build configs
    - Reduce js script file size
    - Add stats
    - Replace uglifyjs to terser

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ DOCS_BUILD_DIR := $(DOCS_DIR)/_build
 .PHONY: build
 build:
 	@make clean
-	@npm run-script package
+	@npm run-script build
 	@python setup.py sdist bdist_wheel
 	@twine check dist/*.whl
 	@twine check dist/*.tar.gz
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,56 @@
-let path = require("path");
-let webpack = require("webpack");
+const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: path.join(__dirname, "js", "main.js"),
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "ghscard.js"
-  },
-  devtool: "source-map"
+module.exports = (env, argv) => {
+  const IS_DEVELOPMENT = argv.mode === "development";
+
+  return {
+    entry: path.join(__dirname, "src", "main.ts"),
+    output: {
+      library: "ghscard",
+      libraryTarget: "umd",
+      path: path.join(__dirname, "dist"),
+      filename: IS_DEVELOPMENT ? "ghscard.js" : "ghscard.min.js"
+    },
+    devtool: IS_DEVELOPMENT ? "source-map" : "none",
+    externals: {
+      jquery: "jQuery"
+    },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: "disabled",
+        generateStatsFile: true,
+        statsFilename: IS_DEVELOPMENT ? "stats-dev.json" : "stats.json",
+        statsOptions: { source: false }
+      })
+    ],
+    optimization: {
+      minimizer: IS_DEVELOPMENT
+        ? []
+        : [
+          new TerserPlugin({
+            terserOptions: {
+              compress: { drop_console: true },
+            },
+            extractComments: "all",
+          })
+        ]
+    },
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        use: "ts-loader"
+      }]
+    },
+    resolve: {
+      modules: [
+        "node_modules",
+      ],
+      extensions: [
+        ".ts",
+        ".js"
+      ]
+    }
+  }
 };
